a8ca869cb7f9122b1ae8bc302a38f7753da52027
Cristian031 <magallanesefidani@gmail.com>
2023-10-13 18:22:22 -0300

    Completoo

--- src/main/java/com/example/demo/services/PersonaService.java
+++ src/main/java/com/example/demo/services/PersonaService.java
@@ -6,7 +6,7 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-public interface PersonaService extends BaseService<Persona, Long>{
+public interface  PersonaService extends BaseService<Persona, Long>{
     List<Persona> search(String filtro) throws Exception;
     Page<Persona> search(String filtro, Pageable pageable) throws Exception;
 }
